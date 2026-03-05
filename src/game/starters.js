f5149be41af3d00e09330fa1795b5a8218961e2c
Gabriel Araujo <ujo.gabri.pro@gmail.com>
2026-03-04 21:32:15 -0300

    feat: add starters

---
+++ src/game/starters.js
@@ -0,0 +1,13 @@
+const STARTERS = {
+  1: ["bulbasaur", "charmander", "squirtle"],
+  2: ["chikorita", "cyndaquil", "totodile"],
+  3: ["treecko", "torchic", "mudkip"],
+  4: ["turtwig", "chimchar", "piplup"],
+  5: ["snivy", "tepig", "oshawott"],
+  6: ["chespin", "fennekin", "froakie"],
+  7: ["rowlet", "litten", "popplio"],
+  8: ["grookey", "scorbunny", "sobble"],
+  9: ["sprigatito", "fuecoco", "quaxly"]
+}
+
+module.exports = { STARTERS }
